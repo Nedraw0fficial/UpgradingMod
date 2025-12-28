5dae927df96b6a771b65f7ea1888d2816f09a2b8
Nedraw_ <warperoriginal@gmail.com>
2025-12-28 21:42:11 +0100

    First disk and registry system

---
+++ src/main/java/com/nedraw/upgrading/client/ClientSetup.java
@@ -0,0 +1,16 @@
+package com.nedraw.upgrading.client;
+
+import com.nedraw.upgrading.UpgradingMod;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.client.event.RegisterKeyMappingsEvent;
+
+@EventBusSubscriber(modid = UpgradingMod.MODID, value = Dist.CLIENT, bus = EventBusSubscriber.Bus.MOD)
+public class ClientSetup {
+
+    @SubscribeEvent
+    public static void registerKeyBinds(RegisterKeyMappingsEvent event) {
+        event.register(ModKeyBinds.OPEN_DISK_MENU);
+    }
+}
---
+++ src/main/java/com/nedraw/upgrading/client/ModKeyBinds.java
@@ -0,0 +1,18 @@
+package com.nedraw.upgrading.client;
+
+import com.mojang.blaze3d.platform.InputConstants;
+import net.minecraft.client.KeyMapping;
+import net.neoforged.neoforge.client.settings.KeyConflictContext;
+import org.lwjgl.glfw.GLFW;
+
+public class ModKeyBinds {
+    public static final String KEY_CATEGORY = "key.categories.upgrading";
+
+    public static final KeyMapping OPEN_DISK_MENU = new KeyMapping(
+            "key.upgrading.open_disk_menu",
+            KeyConflictContext.IN_GAME,
+            InputConstants.Type.KEYSYM,
+            GLFW.GLFW_KEY_R,
+            KEY_CATEGORY
+    );
+}
